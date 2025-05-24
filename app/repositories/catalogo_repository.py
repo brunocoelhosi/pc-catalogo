97fec513fd46585394949f2ce24bb15f34142e29
Isabella Ramos Carvalho <au.isabella.r.c@hotmail.com>
2025-05-24 13:47:01 -0300

    refactor: retirada de metodo não utilizado

--- app/repositories/catalogo_repository.py
+++ app/repositories/catalogo_repository.py
@@ -8,15 +8,7 @@ from .base import AsyncMemoryRepository
 
 class CatalogoRepository(AsyncMemoryRepository[Catalogo, UUID]):
 
-    async def find_by_name(self, name: str) -> Catalogo:
-        """
-        Busca um alguma coisa pelo nome.
-        """
-        result = next((s for s in self.memory if s["name"] == name), None)
-        if result:
-            return result
-        raise NotFoundException()
-
+     """ """
 
 __all__ = ["CatalogoRepository"]
     
